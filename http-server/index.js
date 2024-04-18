2b9b06c36259474bbbb1fa607539c9e5c86606a6
Hasika <hasika20.2005@gmail.com>
2024-04-18 19:43:57 +0530

    Level 2 MileStone

---
+++ http-server/index.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+const argv = require('minimist')(process.argv.slice(2));
+
+const port = argv.port || 3000;
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = registration;
+});
+
+http.createServer((req, res) => {
+    let url = req.url;
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    switch(url) {
+        case "/project":
+            res.write(projectContent);
+            res.end();
+            break;
+        case "/registration":
+            res.write(registrationContent);
+            res.end();
+            break;
+        default:
+            res.write(homeContent);
+            res.end();
+            break;
+    }
+})
+.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
