ec10f09834a2b02338171389c3b430887bba89d2
Hasika <hasika20.2005@gmail.com>
2024-04-25 21:48:53 +0530

    Milestone 6

--- todo-app/__tests__/todo.js
+++ todo-app/__tests__/todo.js
@@ -1,10 +1,11 @@
 const request = require("supertest");
+
 const db = require("../models/index");
 const app = require("../app");
 
 let server, agent;
 
-describe("Todo Test Suite", () => {
+describe("Todo Application", function () {
   beforeAll(async () => {
     await db.sequelize.sync({ force: true });
     server = app.listen(3000, () => {});
@@ -12,13 +13,17 @@ describe("Todo Test Suite", () => {
   });
 
   afterAll(async () => {
-    await db.sequelize.close();
-    server.close();
+    try {
+      await db.sequelize.close();
+      await server.close();
+    } catch (error) {
+      console.log(error);
+    }
   });
 
-  test("responds with json at /todos", async () => {
+  test("Creates a todo and responds with json at /todos POST endpoint", async () => {
     const response = await agent.post("/todos").send({
-      title: "Buy Milk",
+      title: "Buy milk",
       dueDate: new Date().toISOString(),
       completed: false,
     });
@@ -29,9 +34,10 @@ describe("Todo Test Suite", () => {
     const parsedResponse = JSON.parse(response.text);
     expect(parsedResponse.id).toBeDefined();
   });
-  test("Marks a todo as complete", async () => {
+
+  test("Marks a todo with the given ID as complete", async () => {
     const response = await agent.post("/todos").send({
-      title: "Buy Milk",
+      title: "Buy milk",
       dueDate: new Date().toISOString(),
       completed: false,
     });
@@ -41,9 +47,42 @@ describe("Todo Test Suite", () => {
     expect(parsedResponse.completed).toBe(false);
 
     const markCompleteResponse = await agent
-      .put(`/todos/${todoID}/markAsCompleted`)
+      .put(`/todos/${todoID}/markASCompleted`)
       .send();
     const parsedUpdateResponse = JSON.parse(markCompleteResponse.text);
     expect(parsedUpdateResponse.completed).toBe(true);
+  });
+
+  test("Fetches all todos in the database using /todos endpoint", async () => {
+    await agent.post("/todos").send({
+      title: "Buy xbox",
+      dueDate: new Date().toISOString(),
+      completed: false,
+    });
+    await agent.post("/todos").send({
+      title: "Buy ps3",
+      dueDate: new Date().toISOString(),
+      completed: false,
+    });
+    const response = await agent.get("/todos");
+    const parsedResponse = JSON.parse(response.text);
+
+    expect(parsedResponse.length).toBe(4);
+    expect(parsedResponse[3]["title"]).toBe("Buy ps3");
+  });
+
+  test("Deletes a todo with the given ID if it exists and sends a boolean response", async () => {
+    // Create a new todo to delete
+    const createResponse = await agent.post("/todos").send({
+      title: "Todo to delete",
+      dueDate: new Date().toISOString(),
+      completed: false,
+    });
+    const parsedCreateResponse = JSON.parse(createResponse.text);
+    const todoID = parsedCreateResponse.id;
+
+    const deleteResponse = await agent.delete(`/todos/${todoID}`).send();
+    const parsedTodoDeleteResponse = JSON.parse(deleteResponse.text);
+    expect(parsedTodoDeleteResponse).toBe(true);
   });
 });
--- todo-app/app.js
+++ todo-app/app.js
@@ -1,48 +1,75 @@
-/* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("hello, world!");
+app.get("/", function (request, response) {
+  response.send("Hello World");
 });
 
-app.get("/todos", (req, res) => {
-  console.log("Todo List");
-});
+app.get("/todos", async function (request, response) {
+  console.log("Processing list of all Todos ...");
+  // FILL IN YOUR CODE HERE
 
-app.post("/todos", async (req, res) => {
-  console.log("Create a todo", req.body);
-  //Todo
+  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
+  // Then, we have to respond with all Todos, like:
+  // response.send(todos)
   try {
-    const todo = await Todo.addTodo({
-      title: req.body.title,
-      dueDate: req.body.dueDate,
-      completed: false,
-    });
-    return res.json(todo);
+    const todos = await Todo.findAll();
+    return response.json(todos);
   } catch (error) {
     console.log(error);
-    return res.status(422).json(error);
+    return response.status(422).json(error);
   }
 });
 
-app.put("/todos/:id/markAsCompleted", async (req, res) => {
-  console.log("We have to update a todo with ID:", req.params.id);
-  const todo = await Todo.findByPk(req.params.id);
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.post("/todos", async function (request, response) {
+  try {
+    const todo = await Todo.addTodo(request.body);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async function (request, response) {
+  const todo = await Todo.findByPk(request.params.id);
   try {
     const updatedTodo = await todo.markAsCompleted();
-    return res.json(updatedTodo);
+    return response.json(updatedTodo);
   } catch (error) {
     console.log(error);
-    return res.status(422).json(error);
+    return response.status(422).json(error);
   }
 });
 
-app.delete("/todos/:id", async (req, res) => {
-  console.log("Delete a todo by Id:", req.params.id);
+app.delete("/todos/:id", async function (request, response) {
+  console.log("We have to delete a Todo with ID: ", request.params.id);
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our database to delete a Todo by ID.
+  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
+  // response.send(true)
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    await todo.destroy();
+    response.send(todo ? true : false);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 module.exports = app;
